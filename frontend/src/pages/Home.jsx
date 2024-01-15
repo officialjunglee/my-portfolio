365a36413f054ad4abee64ef095c514b384f1e54
Atishay Khanna <30352985+officialjunglee@users.noreply.github.com>
2024-01-14 22:45:59 -0500

    Update Home.jsx with prod url

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5500/blogs')
+      .get('http://my-portfolio-backend-kohl.vercel.app/blogs')
       .then((response) => {
         setBlogs(response.data);
         console.log(response.data);
