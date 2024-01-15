d5203782fc97abd5a51940a1a5b5c8734406aa43
Atishay Khanna <30352985+officialjunglee@users.noreply.github.com>
2024-01-15 01:04:02 -0500

    Update Home.jsx backend url fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('https://atishay-khanna.vercel.app/blogs')
+      .get('https://my-portfolio-backend-kohl.vercel.app/blogs')
       .then((response) => {
         setBlogs(response.data);
         console.log(response.data);
